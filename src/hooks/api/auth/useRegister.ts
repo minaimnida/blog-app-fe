a0d04871b9f3154562f264e09132dce6a42b28d0
minaimnida <imaduddinmuhammad92@gmail.com>
2025-04-26 20:51:40 +0700

    feat: login and keep login

--- src/hooks/api/auth/useRegister.ts
+++ src/hooks/api/auth/useRegister.ts
@@ -4,15 +4,18 @@ import { axiosInstance } from "@/lib/axios";
 import { User } from "@/types/user";
 import { useMutation } from "@tanstack/react-query";
 import { Axios, AxiosError } from "axios";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 const useRegister = () => {
+  const router = useRouter()
   return useMutation({
     mutationFn: async (payload: Omit<User, "id">) => {
         const {data} = await axiosInstance.post("/auth/register", payload)
     },
     onSuccess: () => {
       toast.success("Register Success");
+      router.push("/login")
     },
     onError: (error: AxiosError<any>) => {
       toast.error(error.response?.data.message);
